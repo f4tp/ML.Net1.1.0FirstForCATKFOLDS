e7501621981b766e2a18d09ab37e55d896f37fca
f4tp <mr_treadwell@live.co.uk>
2019-07-01 09:28:24 +0100

    Add project files.

---
+++ ML1.1.0RaschIrtUserAbilAndItemDiffOnlyKFOLDS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
