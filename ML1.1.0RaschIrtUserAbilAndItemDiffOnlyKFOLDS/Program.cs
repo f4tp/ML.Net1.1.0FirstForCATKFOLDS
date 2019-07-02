17ce5084924b577f0fa0490d91db98fd77050824
f4tp <mr_treadwell@live.co.uk>
2019-07-02 11:14:30 +0100

    @DONE: saves model to zip file @TODO: transformed data (2) needs to be injected through the different method calls, as for save model, it uses class declared variable

--- ML1.1.0RaschIrtUserAbilAndItemDiffOnlyKFOLDS/Program.cs
+++ ML1.1.0RaschIrtUserAbilAndItemDiffOnlyKFOLDS/Program.cs
@@ -28,6 +28,9 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
        
         static readonly string _dataPathNewAgain = Directory.GetParent(_dataPathNew).Parent.FullName;
         static readonly string _dataPath = Path.Combine(_dataPathNewAgain, "Data", "KFoldDatausing1.00etcJustIrtStuff.txt");
+        static readonly string _modelPath = Path.Combine(_dataPathNewAgain, "Data", "FafmIrtBasicData.zip");
+
+        static IDataView transformedDataStage2;
 
         //where the model will be saved when created, but this does not exist until data has been trained on
 
@@ -42,13 +45,14 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
             //now gets standard data returned, not split data
             var splitDataView = LoadData(mlContext);
             //variable below is now not just a model, but a model for each fol, train/test data for each fold, metrcis object for each fold
-            IReadOnlyList<TrainCatalogBase.CrossValidationResult<BinaryClassificationMetrics>> model = BuildAndTrain(mlContext, splitDataView);
+            //IReadOnlyList<TrainCatalogBase.CrossValidationResult<BinaryClassificationMetrics>> model = BuildAndTrain(mlContext, splitDataView);
 
             //vairable below is for the calibrated variant on the variable above
-            //IReadOnlyList<TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics>> model = BuildAndTrain(mlContext, splitDataView);
+            IReadOnlyList<TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics>> model = BuildAndTrain(mlContext, splitDataView);
 
             var topModel = Evaluate(mlContext, model, splitDataView);
             //UseModelWithSingleItem(mlContext, topModel);
+            SaveModelAsFile(mlContext, topModel);
             Console.ReadLine();
 
         }
@@ -66,9 +70,9 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
         }
 
         //calibrated version below
-        //public static IReadOnlyList<TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics>> BuildAndTrain(MLContext mlContext, IDataView splitTrainSet)
+        public static IReadOnlyList<TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics>> BuildAndTrain(MLContext mlContext, IDataView splitTrainSet)
 
-        public static IReadOnlyList<TrainCatalogBase.CrossValidationResult<BinaryClassificationMetrics>> BuildAndTrain(MLContext mlContext, IDataView splitTrainSet)
+        //public static IReadOnlyList<TrainCatalogBase.CrossValidationResult<BinaryClassificationMetrics>> BuildAndTrain(MLContext mlContext, IDataView splitTrainSet)
         {
             //var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(QuestionData.QuestionText))
 
@@ -91,18 +95,18 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
 
             //what does the fit method do? These two methods involve transformign the data for use in the algorithm, unspecific though
             var dataPrepTransformer = transformedDataStage1.Fit(splitTrainSet);
-            IDataView transformedDataStage2 = dataPrepTransformer.Transform(splitTrainSet);
+            transformedDataStage2 = dataPrepTransformer.Transform(splitTrainSet);
 
             //LogReg Stopchastic used as this was given in the sample, which used calibrated model, will change to SVM
 
-            //IEstimator<ITransformer> svmLinAlg = mlContext.BinaryClassification.Trainers.SdcaLogisticRegression();
-            //var cvResults = mlContext.BinaryClassification.CrossValidate(transformedDataStage2, svmLinAlg, numberOfFolds: 10);
+            IEstimator<ITransformer> svmLinAlg = mlContext.BinaryClassification.Trainers.FieldAwareFactorizationMachine();
+            var cvResults = mlContext.BinaryClassification.CrossValidate(transformedDataStage2, svmLinAlg, numberOfFolds: 10);
 
 
             //svm linear now used
 
-            IEstimator<ITransformer> svmLinAlg = mlContext.BinaryClassification.Trainers.FastForest();
-            var cvResults = mlContext.BinaryClassification.CrossValidateNonCalibrated(transformedDataStage2, svmLinAlg, numberOfFolds: 10);
+            //IEstimator<ITransformer> svmLinAlg = mlContext.BinaryClassification.Trainers.FastForest();
+            //var cvResults = mlContext.BinaryClassification.CrossValidateNonCalibrated(transformedDataStage2, svmLinAlg, numberOfFolds: 10);
 
 
             //Console.WriteLine("=============== Create and Train the Model ===============");
@@ -122,7 +126,7 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
-            Console.WriteLine($"====================Time taken: {elapsedTime}===================");
+            Console.WriteLine($"Time taken (hh:mm:ss:msms):  {elapsedTime}");
 
             return cvResults;
 
@@ -130,10 +134,10 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
         }
 
         //calibrated version below
-        //public static ITransformer Evaluate(MLContext mlContext, IReadOnlyList<TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics>> cvResults, IDataView splitTestSet)
+        public static ITransformer Evaluate(MLContext mlContext, IReadOnlyList<TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics>> cvResults, IDataView splitTestSet)
 
         //noncalibrated version below
-        public static ITransformer Evaluate(MLContext mlContext, IReadOnlyList<TrainCatalogBase.CrossValidationResult<BinaryClassificationMetrics>> cvResults, IDataView splitTestSet)
+        //public static ITransformer Evaluate(MLContext mlContext, IReadOnlyList<TrainCatalogBase.CrossValidationResult<BinaryClassificationMetrics>> cvResults, IDataView splitTestSet)
         {
 
             //model contains these things
@@ -158,13 +162,15 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
                 .Select(fold => fold.Model)
                 .ToArray();
             ITransformer topModel = models[0];
-            
-            //call predict with single instance method here using the model that has been brought out
 
+            //call predict with single instance method here using the model that has been brought out
+            double total = 0;
             foreach(double acc in rSquared)
             {
                 Console.WriteLine(acc.ToString());
+                total += acc;
             }
+            Console.WriteLine($"average: {total / 10}");
 
             //Console.WriteLine($"Higest Accuracy: {topModel.ToString()}");
 
@@ -205,6 +211,16 @@ namespace ML1._1._0RaschIrtUserAbilAndItemDiffOnlyKFOLDS
             //Console.WriteLine("=============== End of model evaluation ===============");
 
         }
+
+
+        
+        private static void SaveModelAsFile(MLContext mlContext, ITransformer model)
+        {
+            using (var fs = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                mlContext.Model.Save(model, transformedDataStage2.Schema, fs);
+            Console.WriteLine("The model is saved to {0}", _modelPath);
+        }
+
 
         public static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
         {
